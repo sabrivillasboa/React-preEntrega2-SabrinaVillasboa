f2c81c6341de087535147928fce456b71a23f476
Sabrina <sabrinavillasboa@gmail.com>
2023-10-14 13:20:30 -0300

    navegacion a ItemDetail

--- src/pages/ItemDetail/ItemDetail.jsx
+++ src/pages/ItemDetail/ItemDetail.jsx
@@ -1,26 +1,30 @@
 import { useState, useEffect } from "react";
-
 import { useParams } from "react-router-dom";
-
 import Item from "../../components/Item/Item";
+import { asyncMock } from "../../asyncMock";
+import { dataBooks } from "../dataBooks";
+import { Box } from "@mui/material";
 
-const ItemDetail = () => {
+const ItemDetail = (props) => {
   const { id } = useParams();
 
-  const [item, setItem] = useState([]);
-
-  // console.log(char);
+  const [item, setItem] = useState({});
 
   useEffect(() => {
+
     asyncMock(dataBooks)
-    .then((res) =>setItem(res))
-    .catch((error) => console.log(error));
+    .then((result) => {
+      const filteredElement = result.find(prod => prod.id == id);
+      setItem(filteredElement);
+      console.log(item);
+    }).catch((error) => console.log(error));
   }, [id]);
 
+
   return (
-    <div style={{ display: "flex", justifyContent: "center", margin: 20 }}>
-      {item.id ? <Item product= {product} /> : null}
-    </div>
+    <Box sx={{display: "flex", justifyContent: "center"}}>
+      <Item product={item} />
+    </Box>
   );
 };
 
